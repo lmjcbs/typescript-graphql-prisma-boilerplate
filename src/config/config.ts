7f3b26a5de7923a847b51b219e69980da13dfcc4
lmjcbs <hello@liamjacobs.dev>
2020-10-02 21:08:26 +0100

    add validation to env variables

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import * as Yup from 'yup';
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const envVarsSchema = Yup.object().shape({
+  NODE_ENV: Yup.string().required().oneOf(['development', 'test', 'production']),
+  PORT: Yup.number().required().default(3000),
+});
+
+envVarsSchema.validate(process.env).catch((err: Error) => {
+  console.error(`Config ${err.name}: ${err.message}`);
+});
+
+export const config = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+};
