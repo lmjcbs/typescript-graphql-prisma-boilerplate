7bb7d06db81eece567b11483882a7dbc8cee713b
lmjcbs <hello@liamjacobs.dev>
2020-10-06 20:10:00 +0100

    setup apollo-server-express

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
 import cors from 'cors';
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
 
 const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
 
 // enable CORS
 app.use(cors());
